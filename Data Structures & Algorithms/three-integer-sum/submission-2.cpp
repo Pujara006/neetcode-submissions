455c16c826d9c3cb48b8fab66ee06d4b6f991443
Pujara006 <pujarakanishka@gmail.com>
2026-05-19 00:52:51 +0530

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> triplets;
+        sort(nums.begin(),nums.end());
+        int numSize = nums.size();
+        for(int lo=0;lo<(numSize-2);lo++){
+            if(nums[lo]>0) continue;
+            if(lo>0&&(nums[lo] == nums[lo-1])) continue;
+            int mid = lo+1,hi = numSize-1;
+            while(mid<hi){
+                if(mid>lo+1&&(nums[mid]==nums[mid-1])){
+                    mid++;
+                    continue;
+                }
+                int sum = nums[mid]+nums[hi];
+                if(nums[lo]*(-1)==sum){
+                    triplets.push_back({nums[lo],nums[mid],nums[hi]});
+                    mid++;
+                    hi--;
+                }
+                else if(nums[lo]*(-1)<sum) hi--;
+                else mid++;
+            }
+        }
+        return triplets;
+    }
+};
